3b8452d002df88e8a91828a29d3162582f52a9da
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-27 16:16:25 -0700

    Added states to blog to toggle visibility

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>{blog.title} - {blog.author} <button onClick={toggleVisibility}>{visible ? "Hide" : "View"}</button></p>
+      {visible &&
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes} <button>Like</button></p>
+          <p>{blog.user ? blog.user.name : ''}</p>
+        </div>
+      }
+    </div>
+  )
+}
+
+
 
 export default Blog

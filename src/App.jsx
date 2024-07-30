efa856c773a976d8b015569a0ee9ea0937df6a26
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-07-30 09:49:28 -0700

    Sorts Blogs by Likes

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs(blogs)
+      setBlogs(blogs.sort((a, b) => b.likes - a.likes))
     )
   }, [])
 
@@ -61,6 +61,7 @@ const App = () => {
       const updatedBlog = await blogService.updateBlog(blogToUpdate)
       updatedBlog.user = userInfo
       const updatedBlogs = blogs.map(blog => blog.id === updatedBlog.id ? updatedBlog : blog)
+      updatedBlogs.sort((a, b) => b.likes - a.likes)
       setBlogs(updatedBlogs)
     } catch (e) {
       setErrorMessage('Failed to Update')
